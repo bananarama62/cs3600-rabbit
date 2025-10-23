9d65315cd55cfffac8bdf26a5bb7ad24b10ce916
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-10-23 14:17:24 -0700

    Changed titles

--- database/login.php
+++ database/login.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cookies</title>
+    <title>Login</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../CSS/style.css">
